4443ee18e94f629bb6bf5e4502d644e974a59f18
RitikaOswal21 <159769366+RitikaOswal21@users.noreply.github.com>
2024-02-16 11:52:13 +0530

    Add files via upload

---
+++ Task2_Validation/src/main/java/com/example/springboot/project0/entity/Department.java
@@ -0,0 +1,74 @@
+package com.example.springboot.project0.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Department {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
+	private Long departmentId;
+	
+	@NotBlank(message="please add department name ")
+	private String departmentName;
+	
+	@Pattern(regexp = "[a-zA-Z0-9]+")
+	private String departmentAddress;
+	
+	@NotNull
+	@Size(min=02 , max=50)
+	private String departmentCode;
+	
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
+	public String getDepartmentName() {
+		return departmentName;
+	}
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	public String getDepartmentAddress() {
+		return departmentAddress;
+	}
+	public void setDepartmentAddress(String departmentAddress) {
+		this.departmentAddress = departmentAddress;
+	}
+	public String getDepartmentCode() {
+		return departmentCode;
+	}
+	public void setDepartmentCode(String departmentCode) {
+		this.departmentCode = departmentCode;
+	}
+	
+	public Department(Long departmentId, String departmentName, String departmentAddress, String departmentCode) {
+		super();
+		this.departmentId = departmentId;
+		this.departmentName = departmentName;
+		this.departmentAddress = departmentAddress;
+		this.departmentCode = departmentCode;
+	}
+	public Department() {
+	
+	}
+	@Override
+	public String toString() {
+		return "Department [departmentId=" + departmentId + ", departmentName=" + departmentName
+				+ ", departmentAddress=" + departmentAddress + ", departmentCode=" + departmentCode + "]";
+	}
+	
+	
+	
+
+}
---
+++ Task2_Validation/src/main/java/com/example/springboot/project0/service/DepartmentService.java
@@ -0,0 +1,19 @@
+package com.example.springboot.project0.service;
+import java.util.List;
+
+import  com.example.springboot.project0.entity.Department;
+
+public interface DepartmentService {
+	public Department saveDepartment(Department department);
+
+	public List<Department> fetchDepartmentList();
+
+	public Department fetchDepartmentById(Long departmentId);
+
+	public void deletedepartmentById(Long departmentId);
+
+	public Department updateDepartment(Long departmentId, Department department);
+
+	public Department fetchDepartmentByName(String departmentName);
+
+}
